d1326e32f617936f09bd3cf265cda98d92aa8175
Fv-store <141719506+Fv-store@users.noreply.github.com>
2023-09-25 19:17:32 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/sampiiiiu
+# SL
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -11,20 +11,21 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
+# Getting
 clear
 IP=$(wget -qO- ipinfo.io/ip);
-domain=$(cat /etc/xray/domain)
 date=$(date +"%Y-%m-%d")
 clear
-email=$(cat /root/email)
+email=$(cat /home/email)
 if [[ "$email" = "" ]]; then
 echo "Masukkan Email Untuk Menerima Backup"
 read -rp "Email : " -e email
-cat <<EOF>>/root/email
+cat <<EOF>>/home/email
 $email
 EOF
 fi
 clear
+figlet "Backup"
 echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
 rm -rf /root/backup
 mkdir /root/backup
@@ -32,10 +33,8 @@ cp /etc/passwd backup/
 cp /etc/group backup/
 cp /etc/shadow backup/
 cp /etc/gshadow backup/
-cp /etc/crontab backup/
-cp -r /var/lib/geovpn/ backup/geovpn
 cp -r /etc/xray backup/xray
-cp -r /var/www/html/ backup/html
+cp -r /home/vps/public_html backup/public_html
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
@@ -61,5 +60,6 @@ Link Backup   : $link
 Tanggal       : $date
 ==================================
 "
-echo "Silahkan copy Link dan restore di VPS baru"
-echo ""
+echo "Silahkan cek Kotak Masuk $email"
+read -p "[ENTER] BACK TO MENU"
+menu
